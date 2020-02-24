f248ebc8d25934ea5b46809d9575d240b41a5f2e
anneliese schulte <anneliese.schulte-hordelhoff.2022@student.lhschools.org>
2020-02-24 13:50:09 -0500

    Brought Under Source Control

---
+++ src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        System.out.println("+______________________+");
+        System.out.println("|Shell Gas Station      |");
+        System.out.println("|Address 1              |");
+        System.out.println("|Address 2              |");
+        System.out.println("|Date           Time    |");
+        System.out.println("|14January20    3:23    |");
+        System.out.println("|Invoice#       10101010|");
+        System.out.println("|AUTH#                  |");
+        System.out.println("|      Mastercard       |");
+        System.out.println("|    Account number     |");
+        System.out.println("|   *************15     |");
+        System.out.println("| Pump   PRODUCT   $/G  |");
+        System.out.println("|  01     UNLD     $2.4 |");
+        System.out.println("|Gallons   Fuel   Total |");
+        System.out.println("| 19.87       $   $30.00|");
+        System.out.println("|       THANK YOU       |");
+        System.out.println("|   SHELL GAS STATION   |");
+        System.out.println("|_______________________|");
+
+    }
+}
